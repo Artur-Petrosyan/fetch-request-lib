e26264f6c6064fc4af2117400cf1abc0ce31027a
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-23 22:03:55 +0400

    feat(mode): webpack
    add the config for development and production modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path')
-module.exports = {
-    mode : 'production',
-    entry: './index.js',
-    devServer: {
-        port : 8080,
-        hot : true
-    },
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: "index.js",
-        clean: true,
-    },
+module.exports = function (env, argv) {
+    const isProduction = argv.mode === "production"
+    return {
+        entry: './index.js',
+        devServer: {
+            port: 8080,
+            hot: true
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isProduction ? 'eazyrequest.min.js' : "eazyrequest.development.js",
+            library: {
+                name : "EazyRequest",
+                type: "umd"
+            },
+            umdNamedDefine: true,
+            clean: true,
+        },
 
+    }
 }
